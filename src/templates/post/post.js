4249fdc1829094d7692e509a6a056932de19f3cc
Reevan <rolwin.reevan@techjini.com>
2020-03-16 14:42:50 +0530

    blog cover and title

--- src/templates/post/post.js
+++ src/templates/post/post.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import { Layout } from 'antd'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import Header from '../../components/PageLayout/Header'
 import SidebarWrapper from '../../components/PageLayout/Sidebar'
 
 import 'prismjs/themes/prism-solarizedlight.css'
 import './highlight-syntax.less'
+import style from'./post.module.less'
 
 const Post = ({ data }) => {
-  const { html } = data.markdownRemark
+  const { html , frontmatter } = data.markdownRemark
+  const { title, cover:{ childImageSharp:{ fluid }}, excerpt } = frontmatter
+
   return <Layout className='outerPadding'>
     <Layout className='container'>
       <Header />
       <SidebarWrapper>
+        <h1>{ title }</h1>
+        <div className={style.bannerImgContainer}>
+          <Img className={style.bannerImg} fluid={fluid} title={excerpt} alt={title} />
+        </div>
+        
         <article dangerouslySetInnerHTML={{ __html: html }} />
       </SidebarWrapper>
     </Layout>
