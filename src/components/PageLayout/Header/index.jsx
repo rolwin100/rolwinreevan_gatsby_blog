0545623734ac7dc0a554785219d4757ddf16b97c
Reevan <rolwin.reevan@techjini.com>
2020-03-17 17:13:23 +0530

    header completed

--- src/components/PageLayout/Header/index.jsx
+++ src/components/PageLayout/Header/index.jsx
@@ -1,18 +1,45 @@
-import  React  from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
 import { Layout } from 'antd'
+import { useState } from 'react'
 import 'font-awesome/less/font-awesome.less'
-import  style from './header.module.less'
+import style from './header.module.less'
 import '../../../styles/global.less'
 import Config from '../../../../config'
 import Utils from '../../../utils'
+import { useWindowSize } from '../../../hooks'
 
-export default () => 
-        <Layout className={ style.navWrap }>
-                <ul className={ style.nav }>
-                    <li className={ style.navItem }><Link to={Utils.resolvePageUrl(Config.pages.home)} activeClassName={style.anchorActive}>About</Link></li>
-                    <li className={ style.navItem }><Link to={`/${Utils.resolvePageUrl(Config.pages.contact)}`} activeClassName={style.anchorActive}>Contact</Link></li>
-                    <li className={ style.navItem }><Link to={`/${Utils.resolvePageUrl(Config.pages.blog)}`} activeClassName={style.anchorActive}>Blog</Link></li>
-                    <li className={ style.navItem }><a href='#about'>Resume</a></li>
-                </ul>
-        </Layout>
+
+
+export default ()=> {
+                let [menu, setMenu ] = useState(false)
+
+                const [ width ] = useWindowSize()
+                const toggleMenu = ()=>{
+                        if(width < 768)
+                                menu ? setMenu( false ) : setMenu( true) 
+                }
+                return <>
+                        <div className={style.circleMenu} onClick={toggleMenu}>
+                                <div className={style.hamburger}>
+                                        <div className={style.hamburgerText}>MENU</div>
+                                </div>
+                        </div>
+                        <Layout className={`${style.navWrap} ${ menu ? null : style.hidden} ${ menu ? style.openMenu : null}`}>
+                                <div className={style.backgroundDiv}>
+                                        <ul className={style.nav}>
+                                                <li className={style.navItem}>
+                                                        <Link to={Utils.resolvePageUrl(Config.pages.home)} onClick={toggleMenu} activeClassName={style.anchorActive}>About</Link>
+                                                </li>
+                                                <li className={style.navItem}>
+                                                        <Link to={`/${Utils.resolvePageUrl(Config.pages.contact)}`} onClick={toggleMenu} activeClassName={style.anchorActive}>Contact</Link>
+                                                </li>
+                                                <li className={style.navItem}>
+                                                        <Link to={`/${Utils.resolvePageUrl(Config.pages.blog)}`}  onClick={toggleMenu} activeClassName={style.anchorActive}>Blog</Link>
+                                                </li>
+                                                <li className={style.navItem}><a href='#about'>Resume</a></li>
+                                        </ul>
+                                </div>
+                        </Layout>
+                </>
+        }
